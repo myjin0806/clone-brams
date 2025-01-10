7a592fe3136e7f234a92fd16ea651ed16c622644
Kim Haejin <myjin0806@gmail.com>
2025-01-10 11:23:43 +0900

    Fix unused useEffect import

--- src/pages/RentalProductsPage.jsx
+++ src/pages/RentalProductsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Title from '../components/common/Title';
 import ProductCard from '../components/common/ProductCard';
 import Products from '../data/Products.json';
